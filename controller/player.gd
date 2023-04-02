ecbb309ead13d0c7139e2fad0e594274c0743540
Vítor de Albuquerque Torreão <torreao@pm.me>
2023-04-02 17:59:57 -0300

    Slow down backward movement

--- controller/player.gd
+++ controller/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 @export var turn_speed: float = 2.0
-@export var move_speed: float = 7.0
+@export var forward_speed: float = 4.0
+@export var backward_speed: float = 2.0
 
 var _turn_input_strength: float
 var _move_input_strength: float
@@ -12,5 +13,8 @@ func _process(delta):
 
 func _physics_process(delta):
 	rotate_y(_turn_input_strength * turn_speed * delta)
-	velocity = global_transform.basis.z * _move_input_strength * move_speed
+	if _move_input_strength < 0:
+		velocity = global_transform.basis.z * _move_input_strength * forward_speed
+	else:
+		velocity = global_transform.basis.z * _move_input_strength * backward_speed
 	move_and_slide()
